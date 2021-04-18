842642ec8014a928a181752ff4f0f9333be61715
Peter Pfeufer <info@ppfeufer.de>
2021-04-18 20:00:28 +0200

    typos fixed

--- aa_theme_slate/__init__.py
+++ aa_theme_slate/__init__.py
@@ -1,4 +1,4 @@
 default_app_config = "aa_theme_slate.apps.AaThemeConfig"
 
-__version__ = "1.0.0"
-__title__ = 'Bootsatrap Theme "Slate" for Alliance Auth'
+__version__ = "1.0.1"
+__title__ = 'Bootstrap Theme "Slate" for Alliance Auth'
--- aa_theme_slate/apps.py
+++ aa_theme_slate/apps.py
@@ -6,6 +6,6 @@ from . import __version__
 class AaThemeConfig(AppConfig):
     name = "aa_theme_slate"
     label = "aa_theme_slate"
-    verbose_name = 'Bootsatrap Theme "Slate" for Alliance Auth v{version}'.format(
+    verbose_name = 'Bootstrap Theme "Slate" for Alliance Auth v{version}'.format(
         version=__version__
     )
