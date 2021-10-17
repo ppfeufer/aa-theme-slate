8648fbba3c3e251e0aa5452a5ed92ba04e79d6c1
Peter Pfeufer <info@ppfeufer.de>
2021-10-17 17:14:23 +0200

    tests fixed for AA 2.9.0 and version set to 1.2.0

--- aa_theme_slate/__init__.py
+++ aa_theme_slate/__init__.py
@@ -4,5 +4,5 @@ Initialize the app
 
 default_app_config: str = "aa_theme_slate.apps.AaThemeConfig"
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __title__ = 'Bootstrap Theme "Slate" for Alliance Auth'
--- setup.py
+++ setup.py
@@ -33,14 +33,14 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    python_requires="~=3.6",
+    python_requires="~=3.7",
     install_requires=[
-        "allianceauth>=2.8.2",
+        "allianceauth>=2.9.0",
     ],
 )
