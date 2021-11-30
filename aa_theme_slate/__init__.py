9be0c2f93fdc6ff768c8cbcc970e403ec125e514
Peter Pfeufer <info@ppfeufer.de>
2021-11-30 09:12:02 +0100

    Minimum requirements updated
      - Python 3.7
      - Alliance Auth v2.9.3

--- aa_theme_slate/__init__.py
+++ aa_theme_slate/__init__.py
@@ -4,5 +4,5 @@ Initialize the app
 
 default_app_config: str = "aa_theme_slate.apps.AaThemeConfig"
 
-__version__ = "1.2.3"
+__version__ = "1.3.0"
 __title__ = 'Bootstrap Theme "Slate" for Alliance Auth'
